b3e97b09bef461f05a3d7d67a0d4149040049ead
zibonbadi <zibonbadi@users.noreply.github.com>
2022-11-13 16:30:00 +0100

    Fix Keymapper.bind()

--- src/keymapper.cc
+++ src/keymapper.cc
@@ -9,8 +9,9 @@ KeyMapper::~KeyMapper(){
 
 int KeyMapper::bind(SDL_Keycode key, Event event){
 	try{
-		this->key_events.at(key) = event;
-		std::clog << "KeyMapper.bind(" << key << "): Bound key " << key << " to \"" << event.get("type") << "\"." << std::endl;
+		//this->key_events.at(key) = event;
+		this->key_events.insert(std::pair<SDL_Keycode, Event>(key,event));
+		std::clog << "KeyMapper.bind(" << key << "): Bound key " << key << " to \"" << this->key_events.at(key).get("type") << "\"." << std::endl;
 		return 0;
 	}catch(std::exception &e){
 		std::cerr << "KeyMapper.bind(" << key << ") exception: " << e.what() << std::endl;
@@ -31,8 +32,15 @@ int KeyMapper::unbind(SDL_Keycode key){
 
 int KeyMapper::probe(SDL_KeyboardEvent sdlevent){
 	try{
-		std::cerr << "KeyMapper.probe(" << sdlevent.keysym.sym << "): Called." << std::endl;
-		this->bus->send(&this->key_events.at(sdlevent.keysym.sym));
+		Event e_send = this->key_events.at(sdlevent.keysym.sym);
+		// status_edge: "up" "down" "on" "off" "toggle"
+		if(sdlevent.repeat == 0 && sdlevent.type == SDL_KEYDOWN){
+			e_send.set("status_edge", "down");
+		}else if(sdlevent.type == SDL_KEYUP){
+			e_send.set("status_edge", "up");
+		}
+			this->bus->send(&e_send);
+			std::cerr << "KeyMapper.probe(" << sdlevent.keysym.sym << "): Called." << std::endl;
 		return 0;
 	}catch (std::exception &e){
 		std::cerr << "KeyMapper.probe() exception: " << e.what() << std::endl;
