86368df28e0f1471c08aca7fa0d8c05a4b746d9a
R4BN <robin.cox@gmx.de>
2022-11-13 14:31:57 +0100

    Player movement

--- src/main.cc
+++ src/main.cc
@@ -74,7 +74,9 @@ int main(int argc, char* argv[]){
 	try{
 		std::cout << "Building system..." << std::endl;
 		Z_RGBA bgcolor = { .r = 0x00, .g = 0x00, .b = 0x00 };
-		Game screen((int)800,(int)800, bgcolor);
+		constexpr int SCREEN_HEIGHT = 800;
+		constexpr int SCREEN_WIDTH = 800;
+		Game screen((int)SCREEN_WIDTH,(int)SCREEN_HEIGHT, bgcolor);
 
 			EventBus ebus;
 			EBus_Fn quit_func = [](Event* e){ running = false; };
@@ -86,29 +88,61 @@ int main(int argc, char* argv[]){
 			keymap.bind(SDLK_q, e_quit);
 
 			ResourceManager rc(screen.getRenderer());
-			rc.import_texture("spritesheet", "./assets/Flowerdefense_Spritesheet.png");
+			/* Import spritesheet */
+			rc.import_texture("spritesheet", "./assets/spritesheet.png");
 
-			
-			auto playerBottom = Animation(rc.get_texture("spritesheet"), 5);
+			/* Select tiles from tileset */
+			auto grass = rc.make_static_sprite_from_texture("tiles.grass", "spritesheet", Z_PlaneMeta{ .u = 32 * 4, .v = 32 * 5, .uw = 32, .vw = 32 }).second;
+			auto rose = rc.make_static_sprite_from_texture("tiles.rose", "spritesheet", Z_PlaneMeta{ .u = 32 * 5, .v = 32 * 5, .uw = 32, .vw = 32 }).second;
 
-			playerBottom.add_frame(Z_PlaneMeta{ .u = 32 * 0, .v = 32 * 1, .uw = 32, .vw = 32 });
-			playerBottom.add_frame(Z_PlaneMeta{ .u = 32 * 1, .v = 32 * 1, .uw = 32, .vw = 32 });
-			playerBottom.add_frame(Z_PlaneMeta{ .u = 32 * 2, .v = 32 * 1, .uw = 32, .vw = 32 });
-			playerBottom.add_frame(Z_PlaneMeta{ .u = 32 * 3, .v = 32 * 1, .uw = 32, .vw = 32 });
-			playerBottom.add_frame(Z_PlaneMeta{ .u = 32 * 4, .v = 32 * 1, .uw = 32, .vw = 32 });
+			/* Player animation definition */
+			auto playerLeft = Animation(rc.get_texture("spritesheet"), 5);
 
-			playerBottom.add_xsheet_phase(0, 1);
-			playerBottom.add_xsheet_phase(1, 1);
-			playerBottom.add_xsheet_phase(2, 1);
-			playerBottom.add_xsheet_phase(3, 1);
-			playerBottom.add_xsheet_phase(4, 1);
-			rc.add_anim("player.down", &playerBottom);
-			auto player = rc.make_sprite_from_anim("player", "player.down", Z_PlaneMeta { .x = 400-64, .y = 200, .w = 128, .h = 128 }).second;
+			playerLeft.add_frame(Z_PlaneMeta{ .u = 0, .v = 32 * 2, .uw = 32, .vw = 32 });
+			playerLeft.add_frame(Z_PlaneMeta{ .u = 32 * 1, .v = 32 * 2, .uw = 32, .vw = 32 });
+			playerLeft.add_frame(Z_PlaneMeta{ .u = 32 * 2, .v = 32 * 2, .uw = 32, .vw = 32 });
+			playerLeft.add_frame(Z_PlaneMeta{ .u = 32 * 3, .v = 32 * 2, .uw = 32, .vw = 32 });
+			playerLeft.add_frame(Z_PlaneMeta{ .u = 32 * 4, .v = 32 * 2, .uw = 32, .vw = 32 });
+
+			playerLeft.add_xsheet_phase(0, 1);
+			playerLeft.add_xsheet_phase(1, 1);
+			playerLeft.add_xsheet_phase(2, 1);
+			playerLeft.add_xsheet_phase(3, 1);
+			playerLeft.add_xsheet_phase(4, 1);
+			playerLeft.add_xsheet_phase(3, 1);
+			playerLeft.add_xsheet_phase(2, 1);
+			playerLeft.add_xsheet_phase(1, 1);
+			rc.add_anim("player.left", &playerLeft);
+
+			auto playerRight = Animation(rc.get_texture("spritesheet"), 5);
+
+			playerRight.add_frame(Z_PlaneMeta{ .u = 0, .v = 32 * 3, .uw = 32, .vw = 32 });
+			playerRight.add_frame(Z_PlaneMeta{ .u = 32 * 1, .v = 32 * 3, .uw = 32, .vw = 32 });
+			playerRight.add_frame(Z_PlaneMeta{ .u = 32 * 2, .v = 32 * 3, .uw = 32, .vw = 32 });
+			playerRight.add_frame(Z_PlaneMeta{ .u = 32 * 3, .v = 32 * 3, .uw = 32, .vw = 32 });
+			playerRight.add_frame(Z_PlaneMeta{ .u = 32 * 4, .v = 32 * 3, .uw = 32, .vw = 32 });
+
+			playerRight.add_xsheet_phase(0, 1);
+			playerRight.add_xsheet_phase(1, 1);
+			playerRight.add_xsheet_phase(2, 1);
+			playerRight.add_xsheet_phase(3, 1);
+			playerRight.add_xsheet_phase(4, 1);
+			playerRight.add_xsheet_phase(3, 1);
+			playerRight.add_xsheet_phase(2, 1);
+			playerRight.add_xsheet_phase(1, 1);
+
+			rc.add_anim("player.right", &playerRight);
+
+
+
+			auto player = rc.make_sprite_from_anim("player", "player.left", Z_PlaneMeta { .x = 0, .y = 0, .w = 128, .h = 128 }).second;
+			/*auto playerSprite = rc.get_sprite("player").second;
+			rc.add_anim("player.right", &playerRight);
+			playerSprite.add_animation("up", rc.get_anim("player.up"));
+			playerSprite.switch_to_anim("player.up");*/
 
 			Tilemap ground(32,32), plants(32,32);
-			auto dirt = rc.make_static_sprite_from_texture("tiles.dirt", "spritesheet", Z_PlaneMeta {.u = 0, .v = 0, .uw = 85, .vw = 85}).second;
-			auto grass = rc.make_static_sprite_from_texture("grass", "spritesheet", Z_PlaneMeta{ .u = 32 * 4, .v = 32 * 5, .uw = 32, .vw = 32 }).second;
-			auto rose = rc.make_static_sprite_from_texture("tiles.flowers.rose", "spritesheet", Z_PlaneMeta {.u = 32*5, .v = 32*5, .uw = 32, .vw = 32}).second;
+
 			
 			// Set backup color for rose tile
 			rose->set_color(Z_RGBA{ .r = 0xff, .g = 0x7f, .b = 0xcf, .a = 0xff});
@@ -138,6 +172,11 @@ int main(int argc, char* argv[]){
 			}
 
 			uint32_t past = 0;
+
+			/* Player data definition */
+			float playerSpeed = 4.f;
+			SDL_FPoint playerCoordinates = { .x = (SCREEN_WIDTH / 2) - 64, .y = (SCREEN_HEIGHT / 2) - 64 - 200 };
+			SDL_FPoint delta = { .x = 0, .y = 0 };
 			while(running){
 				auto now = SDL_GetTicks();
 				//poll();
@@ -157,6 +196,40 @@ int main(int argc, char* argv[]){
 				}
 				}
 				//playerBottom.advance(now);
+				const Uint8* state = SDL_GetKeyboardState(nullptr);
+
+
+				/* Update player */
+				delta.x = 0;
+				delta.y = 0;
+				if (state[SDL_SCANCODE_UP])		delta.y = -1;
+				if (state[SDL_SCANCODE_DOWN])	delta.y = 1;
+				if (state[SDL_SCANCODE_LEFT])	delta.x = -1;
+				if (state[SDL_SCANCODE_RIGHT])	delta.x = 1;
+
+				/* Normalize delta length */
+				if (delta.x != 0 || delta.y != 0)
+				{
+					const float length_inverse = 1.f / sqrt(delta.x * delta.x + delta.y * delta.y);
+					delta.x *= length_inverse * playerSpeed;
+					delta.y *= length_inverse * playerSpeed;
+				}
+
+				/* Add delta to player coordinates */
+				playerCoordinates.x += delta.x;
+				playerCoordinates.y += delta.y;
+
+				/* Handle Offscreen Movement */
+				if (playerCoordinates.y < 0)				playerCoordinates.y += delta.y;
+				if (playerCoordinates.y > SCREEN_HEIGHT)	playerCoordinates.y -= delta.y;
+				if (playerCoordinates.x < 0)				playerCoordinates.x += delta.x;
+				if (playerCoordinates.x > SCREEN_WIDTH)		playerCoordinates.x -= delta.x;
+
+				/* Adjust player sprite position*/
+				player->setTransform(Z_PlaneMeta{ .x = playerCoordinates.x, .y = playerCoordinates.y, .w = 128, .h = 128 });
+
+				/* Advance the player animation */
+				//playerLeft.advance(now);
 				rc.advance_all_anim(now);
 				screen.render();
 				past = now;
