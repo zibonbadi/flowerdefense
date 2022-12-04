a79d13352d94cdd1b966e0d9551de17f63cef4f6
R4BN <robin.cox@gmx.de>
2022-12-04 15:32:01 +0100

    tweaked values for enemypool

--- src/main.cc
+++ src/main.cc
@@ -76,13 +76,21 @@ int main(int argc, char* argv[]) {
 		player.GetSprite()->setCollider(&collide_player);
 
 		
-		const float spawnTime				= 2.0f;
-		const float spawnTimeWaveEndDelta	= -0.2f;
-		const float spawnTimeMin			= 0.2f;
-		const float spawnCount				= 1;
-		const float poolSize				= 100;
-		const float waveTime				= 6.f;
+		//const float spawnTime				= 2.0f;
+		//const float spawnTimeWaveEndDelta	= -0.2f;
+		//const float spawnTimeMin			= 0.2f;
+		//const float spawnCount				= 1;
+		//const float poolSize				= 100;
+		//const float waveTime				= 6.f;
 
+
+		// SUPER WAVE  welle 10, 20, 30, 40
+		const float spawnTime = 2.0f;
+		const float spawnTimeWaveEndDelta = -0.2f;
+		const float spawnTimeMin = 0.2f;
+		const float spawnCount = 20;
+		const float poolSize = 5000;
+		const float waveTime = 6.f;
 		Enemypool enemyPool(board, player, collide_enemy, spawnTime, spawnCount, poolSize);
 
 		float waveTimer = waveTime;
